209a2f9b653b1efc165dc21600bc6d812e486823
Bayakan <spring.boot@hotmail.com>
2024-05-17 19:40:23 +0300

    Практика с массивами. Уроки по Java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,40 @@
 package com.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = new int[10];
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 10; i++) {
+            arr[i] = i;
         }
+        arr[0] = 10;
+        arr[6] = 10;
+
+        int result = arr[0] + arr[6];
+        System.out.println(result);
+
+
+        int res = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                res = res + 10;
+            } else {
+                res = res + i;
+            }
+        }
+        System.out.println(res);
+
+        int[] d = {2, 34, 2, 1};
+        int summa = 0;
+
+        for (int x : d) {
+            summa = summa + x;
+            System.out.print(x + " ");
+
+        }
+        System.out.print(summa + " ");
+
+
     }
 }
+
